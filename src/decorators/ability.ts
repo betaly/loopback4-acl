98e6953fe8d3d1c7597940dcfe3e4ead14bb06d2
TY <towyuan@outlook.com>
2023-09-02 23:20:19 +0800

    chore: refactor

--- src/decorators/ability.ts
+++ src/decorators/ability.ts
@@ -5,34 +5,32 @@ import {AnyClass, AnyObject, SubjectResolver} from '../types';
 
 export type ClassOrMethodDecorator = MethodDecorator & ClassDecorator;
 
-export interface AbilityOptions<Subject = AnyObject> {
+export interface AbilityRule<Subject = AnyObject> {
   action: string;
   subject: string | AnyClass<Subject>;
   subjectResolver?: SubjectResolver<Subject> | Record<string, SubjectResolver<Subject>>;
 }
 
-export function ability<Subject = AnyObject>(options: AbilityOptions<Subject>): ClassOrMethodDecorator;
+export function ability<Subject = AnyObject>(rule: AbilityRule<Subject>): ClassOrMethodDecorator;
 export function ability<Subject = AnyObject>(
   action: string,
   subject: string | AnyClass<Subject>,
   subjectResolver?: SubjectResolver<Subject> | Record<string, SubjectResolver<Subject>>,
 ): ClassOrMethodDecorator;
 export function ability<Subject = AnyObject>(
-  optionsOrAction: AbilityOptions<Subject> | string,
+  ruleOrAction: AbilityRule<Subject> | string,
   subject?: string | AnyClass<Subject>,
   subjectResolver?: SubjectResolver<Subject> | Record<string, SubjectResolver<Subject>>,
 ) {
-  const options = (
-    typeof optionsOrAction === 'string'
-      ? {action: optionsOrAction, subject, subjectResolver: subjectResolver}
-      : optionsOrAction
-  ) as AbilityOptions<Subject>;
+  const rule = (
+    typeof ruleOrAction === 'string' ? {action: ruleOrAction, subject, subjectResolver: subjectResolver} : ruleOrAction
+  ) as AbilityRule<Subject>;
 
   // const sub = typeof options.subject === 'string' ? options.subject : options.subject.name;
   // const act = options.action;
   return authorize({
-    resource: options.subject as string,
-    scopes: [options.action],
-    voters: options.subjectResolver ? [authorizerForSubjectResolvers(options.subjectResolver)] : [],
+    resource: rule.subject as string,
+    scopes: [rule.action],
+    voters: rule.subjectResolver ? [authorizerForSubjectResolvers(rule.subjectResolver)] : [],
   });
 }
