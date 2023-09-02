0ae296536bda5c954349ba7ac72c1446a38c90aa
TY <towyuan@outlook.com>
2023-09-02 23:32:40 +0800

    feat(utils): add null check in toArray function (main)

--- src/utils.ts
+++ src/utils.ts
@@ -10,6 +10,9 @@ export function isSubjectResolver<T>(x: unknown): x is SubjectResolver<T> {
   return (Array.isArray(x) && x.length === 2) || isBindingAddress(x) || typeof x === 'function';
 }
 
-export function toArray<T>(x: T | T[]): T[] {
-  return Array.isArray(x) ? x : [x];
+export function toArray<T>(x: T | T[]): NonNullable<T>[] {
+  if (x == null) {
+    return [];
+  }
+  return (Array.isArray(x) ? x : [x]) as NonNullable<T>[];
 }
